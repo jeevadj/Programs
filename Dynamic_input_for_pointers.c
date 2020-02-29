0213027a935fa28edb141f67974414f83e034de4
jeevadj <30819921+jeevadj@users.noreply.github.com>
2020-02-29 21:42:53 +0530

    Create Dynamic_input_for_pointers.c

---
+++ Dynamic_input_for_pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int *p,*temp,input,count;
+    char a;
+    p = malloc(sizeof(int *));
+    temp = p;
+    count =0;
+    while(scanf("%d%c",&input,&a)){
+        *temp = input;
+        temp++;
+        if(a =='\n') break;
+    }
+    printf("\n Diff = %d ",(temp - p));
+    for(int itr=0; itr < count ; itr++){
+        printf("\n %d -- %d",itr,*(p+itr));
+    }
+    return 0;
+}
